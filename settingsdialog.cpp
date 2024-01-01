543d824c3ed9d4dab8247d744fd2416f4bb97cde
GaetanDC <gaetan.dc@gmail.com>
2024-01-01 18:12:24 +0100

    Improve deprecated QColorDialog

--- settingsdialog.cpp
+++ settingsdialog.cpp
@@ -196,7 +196,8 @@ void SettingsDialog::on_cb_hotKey_stateChanged(int arg1)
 void SettingsDialog::on_pb_warningColor_clicked()
 {
     QSettings settings;
-    QColor c = QColorDialog::getRgba(settings.value(SETTINGS_DUE_WARNING_COLOR,DEFAULT_DUE_WARNING_COLOR).toInt());
+//    QColor c = QColorDialog::getRgba(settings.value(SETTINGS_DUE_WARNING_COLOR,DEFAULT_DUE_WARNING_COLOR).toInt());
+    QColor c = QColorDialog::getColor(QColor::fromRgb(settings.value(SETTINGS_DUE_WARNING_COLOR,DEFAULT_DUE_WARNING_COLOR).toInt()),this);
     settings.setValue(SETTINGS_DUE_WARNING_COLOR,c.rgba());
     updateFonts();
 }
@@ -204,7 +205,8 @@ void SettingsDialog::on_pb_warningColor_clicked()
 void SettingsDialog::on_pb_lateColor_clicked()
 {
     QSettings settings;
-    QColor c = QColorDialog::getRgba(settings.value(SETTINGS_DUE_LATE_COLOR,DEFAULT_DUE_LATE_COLOR).toInt());
+//    QColor c = QColorDialog::getRgba(settings.value(SETTINGS_DUE_LATE_COLOR,DEFAULT_DUE_LATE_COLOR).toInt());
+    QColor c = QColorDialog::getColor(QColor::fromRgb(settings.value(SETTINGS_DUE_LATE_COLOR,DEFAULT_DUE_LATE_COLOR).toInt()),this);
     settings.setValue(SETTINGS_DUE_LATE_COLOR,c.rgba());
     updateFonts();
 

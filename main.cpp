60362baab24a6d03ca8bed85c47627e82afb3f77
GaetanDC <gaetan.dc@gmail.com>
2024-01-27 17:32:44 +0100

    Revert main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <QApplication>
 #include "mainwindow.h"
 
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     a.setWindowIcon(QIcon(":/icons/todour.png"));
     w.show();
+    
     return a.exec();
 }
